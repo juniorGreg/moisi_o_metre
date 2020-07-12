ce9e5e1dd83ec69aa14a0a22bbb2271944eee086
Junior Gregoire <junior.gregoire@gmail.com>
2020-07-12 00:06:26 -0400

    start implemetation of bullshit-o-metre

---
+++ bullshit_o_metre/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BullshitOMetreConfig(AppConfig):
+    name = 'bullshit_o_metre'
---
+++ bullshit_o_metre/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class TaintedLemma(models.Model):
+    TYPE = (
+        ('NEG', 'Negative'),
+        ('NEU', 'Neutral'),
+        ('POS', 'Positive'),
+        ('SUP' 'Superlative')
+    )
+
+    LANG = (
+        ('FR', 'Français'),
+        ('EN', 'English')
+    )
+
+    type = models.CharField(max_length=3, choices=TYPE, default='NEU')
+    lang = models.CharField(max_length=2, choices=LANG, default='FR')
+    name = models.CharField(max_length=400)
+    is_evaluated = models.BooleanField(default=False)
+
+class RecordedWebSite(models.Model):
+    title = models.CharField(max_length=400)
+    url = models.URLField(max_length=400)
+    meaningful_lemmas = models.ManyToManyField(TaintedLemma)
+    lemmas_count = models.PositiveIntegerField()
+
+    score = models.FloatField()
