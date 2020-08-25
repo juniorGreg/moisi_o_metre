f2419f2b715277fb92a228cfce44735f31c341ee
Junior Gregoire <junior.gregoire@gmail.com>
2020-08-25 17:28:16 -0400

    add unique field to url for labeledwebsite

--- bullshit_o_metre/models.py
+++ bullshit_o_metre/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class LabeledWebSite(models.Model):
     title = models.CharField(max_length=400)
-    url = models.URLField(max_length=400)
+    url = models.URLField(max_length=400, unique=True)
     is_bullshit = models.BooleanField()
     texts = models.TextField()
     lang = models.CharField(max_length=5)
